3900632a12d542aec167167bc053b10cf4a9cc5d
gd388 <105911071+Sagararc@users.noreply.github.com>
2023-09-22 13:37:33 +0530

    final

--- core/settings.py
+++ core/settings.py
@@ -1,7 +1,7 @@
 
 
 from pathlib import Path
-
+import asgi
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -62,7 +62,7 @@ TEMPLATES = [
 
 ASGI_APPLICATION = 'core.asgi.application'
 
-WSGI_APPLICATION = 'core.wsgi.application'
+
 
 
 # Database
