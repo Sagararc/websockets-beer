95ce49844414440db9544d864c2148c1f1f2edc8
gd388 <105911071+Sagararc@users.noreply.github.com>
2023-10-18 12:38:31 +0530

    asda

--- app/urls.py
+++ app/urls.py
@@ -1,8 +1,7 @@
 
 from django.contrib import admin
 from django.urls import path
-from .views import FormData,datashow,success
-
+from .views import FormData,datashow
 urlpatterns = [
 
     path('' , FormData , name='form'),
