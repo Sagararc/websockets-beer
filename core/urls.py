314491c2b62af0097e60076e59effce7d722b9e4
gd388 <105911071+Sagararc@users.noreply.github.com>
2023-10-18 12:57:26 +0530

    final

--- core/urls.py
+++ core/urls.py
@@ -1,9 +1,12 @@
 
 from django.contrib import admin
 from django.urls import path,include
-
+from app.views import FormData,datashow,success
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('app.urls'))
+    path('', include('app.urls')),
+    path('' , FormData , name='form'),
+    path('data/' , datashow , name='data'),
+    path('success/' , success , name='success'),
 ]
